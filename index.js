16f80f2787378f0e413e118dedb6cff201938446
heimon1255@gmail.com <heimon28@gmail.com>
2024-04-02 19:48:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+console.log('testing');
+
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen( process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/twitter", (req, res) =>{
+    res.send('heimon twitter')
+ })
